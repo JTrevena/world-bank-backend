8d457bc046b4bcf0868e789feebb639d4cc19ad2
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-05-23 12:37:23 +0100

    Inital server set up

---
+++ server.js
@@ -0,0 +1,15 @@
+import { Application } from "https://deno.land/x/abc/mod.ts";
+import { abcCors } from "https://deno.land/x/cors/mod.ts";
+
+const app = new Application();
+const PORT = 8080;
+
+const CorsSettings = {
+  origin: /^.+localhost:(3000|1234)$/,
+  allowedHeaders: ["Authorization", "Content-Type", "Accept", "Origin", "User-Agent"],
+  credentials: true,
+};
+
+app.use(abcCors(CorsSettings)).start({ port: PORT });
+
+console.log(`Server running on http://localhost:${PORT}`);
