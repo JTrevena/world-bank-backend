39dbda6f5926b5796f8de656966e9563c5442c46
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-05-25 17:55:02 +0100

    Using dollar signs instead of question marks

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ async function postNewUser(server) {
   const salt = await bcrypt.genSalt(8);
   const hashed_password = await bcrypt.hash(password, salt);
 
-  // try {
-  await client.queryObject(
-    "INSERT INTO users (username, hashed_password, salt, admin_permission, created_at) VALUES (?,?,?,?, NOW())",
-    [username, hashed_password, salt, false]
-  );
-  // } catch (e) {
-  //   return server.json({ error: e }, 500);
-  // }
+  try {
+    await client.queryObject(
+      "INSERT INTO users (username, hashed_password, salt, admin_permission, created_at) VALUES ($1,$2,$3,$4, NOW())",
+      [username, hashed_password, salt, false]
+    );
+  } catch (e) {
+    return server.json({ error: e }, 500);
+  }
 
   server.json({ response: "User added successfully" }, 200);
 }
