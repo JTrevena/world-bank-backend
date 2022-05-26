7d609871a12e2d53478348939cbd4335a7611b1e
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-05-26 17:54:08 +0100

    getUser test

--- server.js
+++ server.js
@@ -181,7 +181,8 @@ async function handleLogout(server) {
 
 async function getUserInfo(server, sessionID) {
   const session = (await client.queryObject("SELECT * FROM sessions WHERE uuid = $1;", sessionID)).rows;
-  return { session: session[0] };
+  if (!session) return {}; // TODO: this should log them out; not that this request should ever be sent without a session id
+
   const user_id = session[0].user_id;
   const user = (await client.queryObject("SELECT * FROM users WHERE id = $1;", user_id)).rows;
   return { user: user[0] };
