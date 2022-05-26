006ce70c8dde1eb6ad0a762550c3d810a4f21d6f
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-05-26 15:15:22 +0100

    server json for testing

--- server.js
+++ server.js
@@ -140,18 +140,16 @@ async function getHistory(server) {
   const username = cookies.username; //username is read properly
   const user = await getUserInfo(server, username); // but user is not found correctly
 
-  return server.json({ username: username, user: user });
+  let query = `SELECT * FROM search_history`;
+  let searches;
 
-  // let query = `SELECT * FROM search_history`;
-  // let searches;
+  if (!user.admin_permission) {
+    query += ` WHERE user_id = $1;`;
+    searches = (await client.queryObject(query, user.user.id)).rows;
+  } else searches = (await client.queryObject(query)).rows;
 
-  // if (!user.admin_permission) {
-  //   query += ` WHERE user_id = $1;`;
-  //   searches = (await client.queryObject(query, user.id)).rows;
-  // } else searches = (await client.queryObject(query)).rows;
-
-  // if (searches) server.json({ response: searches });
-  // else server.json({ response: "no searches found" });
+  if (searches) server.json({ response: searches });
+  else server.json({ response: "no searches found" });
 }
 
 async function handleLogout(server) {
